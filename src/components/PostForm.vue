60f43e4569fd48e9ab9319926afada00a2c75b69
tanate <tanate.chu@gmail.com>
2020-09-02 20:05:56 +0700

    add postform for add title , body.

---
+++ src/components/PostForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <form v-if="!loading" class="form" v-on:submit="onSubmit">
+        <div class="input-fieId">
+            <label for="title">Title</label>
+            <input type="text"
+                name="title"
+                v-model="title"
+                class="validate"
+            >
+            <span class="helper-text" data-error="Title must not be empty"></span>
+        </div>
+         <div class="input-fieId">
+            <label for="body">Body</label>
+            <input type="text"
+                name="body"
+                v-model="body"
+                class="validate"
+            >
+            <span class="helper-text" data-error="Title must not be empty"></span>
+        </div>
+        <button type="submit" class="waves-effect waves-light btn">Add</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "PostForm",
+    data(){
+        return{
+            loading: false,
+            title: "",
+            body: ""
+        };
+    }
+}
+</script>
+
+<style  scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,75 @@
 <template>
-    <h3>Home Page</h3>
+<div>
+    <div class="row">
+        <div class="col s6">
+            <PostForm/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s6" v-for="(post,index) in posts"
+            v-bind:item="post"
+            :index = "index"
+            :key="post.id"
+        >
+            <div class="card">
+                <div class="card-content">
+                    <p class="card-title">
+                        {{ post.title }} 
+                    </p>
+                    <p class="timestamp">
+                        {{ post.createdAt }}
+                    </p>
+                    <p> {{post.body }} </p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Edit</a>
+                    <a href="#" class="delete-btn">Delete</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 </template>
+
+
+<script>
+
+import PostService from '../PostService';
+import PostForm from '../components/PostForm';
+const postService = new PostService();
+
+export default {
+    name: "Home",
+    components:{
+        PostForm
+    },
+    data(){
+       return {
+           posts: []
+       }
+    },
+    created(){
+        postService.getAllPost()
+        .then(res => {
+            this.posts = res.data;
+            console.log(this.posts);
+        })
+        .catch(err => console.error(err));
+    }
+}
+</script>
+
+<style scoped>
+    .card .card-content .card-title{
+        margin-bottom: 0;
+    }
+    .card .card-content p.timestamp{
+        color: #999;
+        margin-bottom: 10px;
+    }
+    .delete-btn{
+        color: red !important;
+    }
+
+</style>
